d78feb135aee94be2d2046c86ef880fedca0a72e
germanocandido <germanocandido@msn.com>
2022-09-27 21:13:45 -0300

    feat: inserindo imagens

--- moving/src/App.tsx
+++ moving/src/App.tsx
@@ -1,13 +1,12 @@
 import Footer from "./components/Footer";
 import NavBar from "./components/NavBar";
-import ImgTeste from "../src/assets/earth.jpg";
 import "./styles/main.css";
 function App() {
   return (
     <div>
       <NavBar />
-      <div className="w-36">
-        <img src="../src/assets/earth.jpg" alt="teste" />
+      <div className="w-full">
+        <img src="../src/assets/bg1.png" alt="teste" />
       </div>
       <Footer />
     </div>
